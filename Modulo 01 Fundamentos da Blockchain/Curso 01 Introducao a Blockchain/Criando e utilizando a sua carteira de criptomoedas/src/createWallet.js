65c4978c3ad28dfe87d061c5067a5fe994e0b910
mauripidev <Mauripi@gmail.com>
2026-01-21 12:10:27 -0300

    Refactor createWallet.js for SegWit address generation
    Updated wallet creation logic to use P2SH-P2WPKH format and improved comments.

--- Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
+++ Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
@@ -1,33 +1,33 @@
-//importando as dependencias
-const bip32 = require('bip32')
-const bip39 = require('bip39')
-const bitcoin = require('bitcoinjs-lib')
+const bip32 = require('bip32');
+const bip39 = require('bip39');
+const bitcoin = require('bitcoinjs-lib');
 
-//definir a rede
-//bitcoin - rede principal - mainnet
-//testnet - rede de teste - tesnet
-const network = bitcoin.networks.testnet
+const network = bitcoin.networks.testnet; // Use bitcoin.networks.bitcoin for production
 
-//derivação de carteiras HD
-const path = `m/49'/1'/0'/0` 
+// derivação de carteira Hierárquica Determinística (HD)
+const path = "m/49'/1'/0'/0'"; // Caminho BIP44 para Testnet
 
-//criando o mnemonic para a seed (palavras de senha)
-let mnemonic = bip39.generateMnemonic()
-const seed = bip39.mnemonicToSeedSync(mnemonic)
+//criando o memonic para a seed (palavras de senha)
+let mnemonic = bip39.generateMnemonic(); // Exemplo de frase mnemônica
+const seed = bip39.mnemonicToSeedSync(mnemonic);
 
-//criando a raiz da cartiera HD
-let root = bip32.fromSeed(seed, network)
+//criando a raiz da carteira HD
+const root = bip32.fromSeed(seed, network);
 
-//criando uma conta - par pvt-pub keys
-let account = root.derivePath(path)
-let node = account.derive(0).derive(0)
+//criando uma conta - par de chaves (chave privada e pública)
+let account = root.derivePath(path); // Derivando a primeira conta
+let node = account.derive(0).derive(0); // Derivando o primeiro endereço da conta
 
-let btcAddress = bitcoin.payments.p2pkh({
-    pubkey: node.publicKey,
-    network: network,
-}).address
 
-console.log("Carteira gerada")
-console.log("Endereço: ", btcAddress)
-console.log("Chave privada:", node.toWIF())
-console.log("Seed:", mnemonic)
+//gerando o endereço P2SH-P2WPKH (SegWit)
+const { address } = bitcoin.payments.p2sh({
+  redeem: bitcoin.payments.p2wpkh({ 
+    pubkey: account.publicKey, 
+    network }),
+  network,
+});
+
+console.log("Mnemonic:", mnemonic);
+console.log("Endereço P2SH-P2WPKH:", address);
+console.log("Chave Privada (WIF):", node.toWIF());
+console.log("Chave Pública:", account.publicKey.toString('hex'));
